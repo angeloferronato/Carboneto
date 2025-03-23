86f98ba5e7385ca642551f66f2c874d50d321387
Angelo Ferronato <angeloferronato7@gmail.com>
2025-03-23 14:34:59 -0300

    Iniciando o widget principal

---
+++ lib/widgets/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+      theme: ThemeData(
+        useMaterial3: true,
+        
+      ),
+      
+    );
+  }
+}
